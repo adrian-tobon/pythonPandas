fe8620c2f1e658e34e652b5a9e062da5f977d506
ctobonpe <ctobonpe@gmail.com>
2025-09-07 09:52:43 -0500

    Manejo de datos

--- ManejoDatos.py
+++ ManejoDatos.py
@@ -1,2 +1,19 @@
 import pandas as pd
+#import os
 
+#carga de datos CSV
+#print(os.getcwd())
+data = pd.read_csv('pythonPandas/ejemplo_datos.csv')
+
+#df = pd.DataFrame(data)
+#print(df)
+
+print(data.head())
+print(data.shape)
+print(data.info())
+print(data.describe())
+
+data.to_csv('pythonPandas/new_data.csv')
+
+
+    
